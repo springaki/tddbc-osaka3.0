67f14362d9090022f6963e5eb783611dde88c073
print7 <print7.gm@gmail.com>
2013-01-12 14:57:00 +0900

    テストのリファクタリング

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -8,18 +8,23 @@ describe VendingMachine do
     @vending_machine = VendingMachine.new
   end
 
-  it "金額未投入の場合、総計を取得すると0円が返却される" do
-    @vending_machine.total_amount.should eq(0)
+  context "金額未投入の場合" do
+    it "総計を取得すると0円が返却される" do
+      @vending_machine.total_amount.should eq(0)
+    end
   end
 
-  it "100円を投入できる" do
-    @vending_machine.insert(100)
-    @vending_machine.total_amount.should eq(100)
-  end
 
-  it "投入は複数回できる" do
-    @vending_machine.insert(100)
-    @vending_machine.insert(1000)
-    @vending_machine.total_amount.should eq(1100)
-  end
+  context "金額投入してる場合" do
+    it "100円を投入できる" do
+      @vending_machine.insert(100)
+      @vending_machine.total_amount.should eq(100)
+    end
+
+    it "投入は複数回できる" do
+      @vending_machine.insert(100)
+      @vending_machine.insert(1000)
+      @vending_machine.total_amount.should eq(1100)
+    end
+  end  
 end
