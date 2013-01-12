34a23febcc6ec6ef11662f977b69178d00e4f7ab
print7 <print7.gm@gmail.com>
2013-01-12 14:16:08 +0900

    インサートのテスト追加

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 
 require_relative "../vending_machine"
+
 describe VendingMachine do
   it "金額未投入の場合、総計を取得すると0円が返却される" do
     vending_machine = VendingMachine.new
     vending_machine.total_amount.should eq(0)
+  end
+
+  it "100円を投入できる" do
+    vending_machine = VendingMachine.new
+    vending_machine.insert(100)
+    vending_machine.total_amount.should eq(100)
   end
 end
