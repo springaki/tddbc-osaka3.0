42e8cefa3ed9384333494cc355d61595925aebbf
akahane92 <kakahane@gmail.com>
2013-01-12 15:53:30 +0900

    テスト実装：払い戻しを行うと、投入金額の合計が0円になる

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -38,16 +38,17 @@ describe VendingMachine do
       @vending_machine.total_amount.should eq(1660)
     end
 
-    it "1100円を投入して払い戻しを行うと、1100円が返却される" do
-      @vending_machine.insert(100)
-      @vending_machine.insert(1000)
-      @vending_machine.payback.should eq(1100)
-    end
-
-    it "払い戻しを行うと、0円が返却される" do
-      @vending_machine.insert(100)
-      @vending_machine.payback
-      @vending_machine.payback.should eq(0)
+    context "100円を投入して払い戻しをした場合" do
+      before do
+        @vending_machine.insert(100)
+      end
+      it "100円が返却される  " do
+        @vending_machine.payback.should eq(100)
+      end
+      it "払い戻し後は、0円が返却される" do
+        @vending_machine.payback
+        @vending_machine.payback.should eq(0)
+      end
     end
   end
 end
